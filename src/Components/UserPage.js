fd9c93b2ab8372051b3bef47e1949e18dd572f9c
Nameyray <namibaracheal@gmail.com>
2023-02-16 11:40:42 +0300

    create a user and repos hook

--- src/Components/UserPage.js
+++ src/Components/UserPage.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../Components/Navbar'
 import Search from '../Components/Search'
 import UserInfo from '../Components/UserInfo'
 import Repository from '../Components/Repository'
+import {getUserData, getUserRepo} from '../api/Api'
 
 
 
 function UserPage() {
+
+  //create a user and repo hook
+  const[user, setUser] = useState();
+  const[repos, setRepos] = useState();
   return (
     <div className='app'>
       <Navbar />
